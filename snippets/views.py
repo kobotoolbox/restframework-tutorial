ef60e152227cf2403d3bda119eddb281623bcad8
Alex Dorey <dorey415@gmail.com>
2015-02-11 13:12:28 -0500

    [3] relying entirely on generics (max simplicity of views.py)

--- snippets/views.py
+++ snippets/views.py
@@ -1,30 +1,10 @@
-from django.http import Http404
-
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
